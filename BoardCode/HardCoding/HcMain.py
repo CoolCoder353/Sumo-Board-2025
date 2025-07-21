e475098db97ad13599e660f8dea55b45901e2746
CoolCoder64 <github@otmfamily.com>
2025-07-21 20:30:51 +1000

    Pins update + Motor change

--- BoardCode/HardCoding/HcMain.py
+++ BoardCode/HardCoding/HcMain.py
@@ -2,49 +2,80 @@ import wiringpi as w
 import logging
 import sys
 
-startbutton = 0
+startbutton = 14
 
-leftIR = 0
-rightIR = 0
-frontLeftIR = 0
-frontRightIR = 0
+leftIR = 13
+rightIR = 24
+frontLeftIR = 12
+frontRightIR = 23
 
-colorLeft = 0
-colorRight = 0
+colorLeft = 25
+colorRight = 26
 
-leftMotor = 0
-rightMotor = 0
+# Motor pins
+m1a = 17
+m1b = 19
+m2a = 18
+m2b = 16
 
-
-turnTimerleft = 0
+turnTimerLeft = 0
 turnTimerRight = 0
 turningTime = 25
 
 w.wiringPiSetupGpio()  # For GPIO pin numbering
 
-w.pinMode(leftIR, 0)       # Set pin 6 to 0 ( INPUT )
-w.pinMode(rightIR, 0)       # Set pin 6 to 0 ( INPUT )
-w.pinMode(colorLeft, 0)       # Set pin 6 to 0 ( INPUT )
-w.pinMode(colorRight, 0)       # Set pin 6 to 0 ( INPUT )
-w.pinMode(frontLeftIR, 0)
-w.pinMode(frontRightIR, 0)
-w.pinMode(startbutton, 0)  # Set pin 8 to 0 ( INPUT )
+# Setup input pins
+w.pinMode(leftIR, 0)       # Set to INPUT
+w.pinMode(rightIR, 0)      # Set to INPUT
+w.pinMode(colorLeft, 0)    # Set to INPUT
+w.pinMode(colorRight, 0)   # Set to INPUT
+w.pinMode(frontLeftIR, 0)  # Set to INPUT
+w.pinMode(frontRightIR, 0) # Set to INPUT
+w.pinMode(startbutton, 0)  # Set to INPUT
 
-w.pinMode(leftMotor, 1)     # Set pin 17 to 1 ( OUTPUT )
-w.pinMode(rightMotor, 1)    # Set pin 18 to 1 ( OUTPUT )
+# Setup motor output pins
+w.pinMode(m1a, 1)     # Set to OUTPUT
+w.pinMode(m1b, 1)     # Set to OUTPUT
+w.pinMode(m2a, 1)     # Set to OUTPUT
+w.pinMode(m2b, 1)     # Set to OUTPUT
 
-def getSensorData ():
+def getSensorData():
     p_leftIR = w.digitalRead(leftIR)
     p_rightIR = w.digitalRead(rightIR)
     p_frontLeftIR = w.digitalRead(frontLeftIR)
     p_frontRightIR = w.digitalRead(frontRightIR)
     p_colorLeft = w.digitalRead(colorLeft)
     p_colorRight = w.digitalRead(colorRight)
-    return p_leftIR,p_rightIR,p_frontLeftIR,p_frontRightIR,p_colorLeft,p_colorRight
+    return p_leftIR, p_rightIR, p_frontLeftIR, p_frontRightIR, p_colorLeft, p_colorRight
+
+def moveForward():
+    w.digitalWrite(m1a, 1)
+    w.digitalWrite(m1b, 0)
+    w.digitalWrite(m2a, 1)
+    w.digitalWrite(m2b, 0)
+
+def turnLeft():
+    w.digitalWrite(m1a, 0)
+    w.digitalWrite(m1b, 1)
+    w.digitalWrite(m2a, 1)
+    w.digitalWrite(m2b, 0)
+
+def turnRight():
+    w.digitalWrite(m1a, 1)
+    w.digitalWrite(m1b, 0)
+    w.digitalWrite(m2a, 0)
+    w.digitalWrite(m2b, 1)
+
+def stopMotors():
+    w.digitalWrite(m1a, 0)
+    w.digitalWrite(m1b, 0)
+    w.digitalWrite(m2a, 0)
+    w.digitalWrite(m2b, 0)
 
 while True:
-    if(w.digitalread(startbutton)):
-        p_leftIR,p_rightIR,p_frontLeftIR,p_frontRightIR,p_colorLeft,p_colorRight = getSensorData()
+    if(w.digitalRead(startbutton)):
+        p_leftIR, p_rightIR, p_frontLeftIR, p_frontRightIR, p_colorLeft, p_colorRight = getSensorData()
+        
         if(p_colorLeft == 1 or p_colorRight == 1 or turnTimerLeft > 0 or turnTimerRight > 0):
             #SHIT THERES A LINE... GO BACK
             if(p_colorLeft == 1 or turnTimerLeft > 0):
@@ -53,8 +84,7 @@ while True:
                     turnTimerLeft = turningTime
                 elif turnTimerLeft > 0:
                     turnTimerLeft -= 1
-                w.digitalWrite(leftMotor, 1)
-                w.digitalWrite(rightMotor, 0)
+                turnRight()
               
             elif(p_colorRight == 1 or turnTimerRight > 0):
                 #RIGHT LINE
@@ -62,23 +92,19 @@ while True:
                     turnTimerRight = turningTime
                 elif turnTimerRight > 0:
                     turnTimerRight -= 1
-                
-                w.digitalWrite(leftMotor, 0)
-                w.digitalWrite(rightMotor, 1)
+                turnLeft()
 
         elif(p_frontLeftIR == 1 or p_frontRightIR == 1):
-
             #SHIT WE FOUND THE FUCKER, GET HIM!!!!
             if(p_frontLeftIR == 1):
                 #LEFT FRONT IR
-                w.digitalWrite(leftMotor, 0)
-                w.digitalWrite(rightMotor, 1)
+                turnRight()
             elif(p_frontRightIR == 1):
                 #RIGHT FRONT IR
-                w.digitalWrite(leftMotor, 1)
-                w.digitalWrite(rightMotor, 0)
+                turnLeft()
           
         else:
             #NO LINE, NO PEOPLE, GO FORWARD LONELY BOT
-            w.digitalWrite(leftMotor, 1)
-            w.digitalWrite(rightMotor, 1)
+            moveForward()
+    else:
+        stopMotors()
