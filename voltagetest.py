4e888112ccb8613a0fa935d4c2600cabf4c855df
Tyron Holloway-Hill <94258810+Skitz67@users.noreply.github.com>
2025-07-31 20:14:32 +1000

    oops
    forgot to setup the pins in the test file

--- voltagetest.py
+++ voltagetest.py
@@ -2,7 +2,12 @@ import wiringpi as w
 import time
  
 w.wiringPiSetup()  # For GPIO pin numbering
-w.digitalWrite(25, 1)
+startbuttonread = 27  # Confirmed
+startbuttonwrite = 25 #Confirmed
+w.pinMode(startbuttonread, w.GPIO.INPUT)  # Set to INPUT
+w.pinMode(startbuttonwrite, w.GPIO.OUTPUT) # Set to OUTPUT
+w.digitalWrite(startbuttonwrite, 1)
+
 while True:
     time.sleep(0.1)
     #w.digitalWrite(27, 1)
